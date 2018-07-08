c1f2d6589128fd37243c3159a1af48f0c89f8229
Vitaly <atko@MacBook-Pro-Vitaly.local>
2018-07-08 15:13:33 +0300

    Added text

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Header from './Header';
+import Movie from './Movie';
+import {initialMovies} from '../movies';
+import {additionalMovies} from '../movies';
+import AddMovie from './AddMovie';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      movies: initialMovies
+    };
+    this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
+    this.addMovieToGallery = this.addMovieToGallery.bind( this );
+  }
+
+  loadAdditionalMovies() {
+    var currentMovies = {...this.state.movies};
+    var newMovies = Object.assign(currentMovies, additionalMovies);
+    this.setState({movies: newMovies});
+  }
+
+  addMovieToGallery( movie ) {
+    var ts = Date.now();
+    var newMovie = {};
+    newMovie[ 'movie' + ts ] = movie;
+    var currentMovies = { ...this.state.movies };
+    var newMovies = Object.assign( currentMovies, newMovie );
+    this.setState({ movies: newMovies });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header text="Discover Your Movie Mojo!" />
+        <p className="App-intro">Sharing the a few of our favourite movies</p>
+        <div className="movies">
+          {
+            Object
+            .keys(this.state.movies)
+            .map(key => <Movie key={key} meta={this.state.movies[key]} />)
+          }
+        </div>
+        <div className="add-movies"><button onClick={this.loadAdditionalMovies}>Load more...</button></div>
+        <AddMovie addMovie={this.addMovieToGallery} />
+      </div>
+    );
+  }
+}
+
+export default App;
